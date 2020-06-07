5346360ef5630b80f8f5d8f59639478e120b285a
vishalbpatil1 <52704376+vishalbpatil1@users.noreply.github.com>
2020-06-07 19:51:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ fig = px.scatter(df, x="sepal_width", y="sepal_length")
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+
 app = dash.Dash(__name__)
+server=app.server
 app.layout = html.Div([ dcc.Graph(figure=fig)])
 if __name__ == '__main__':
     app.run_server(debug=Flase)
