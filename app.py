28b6fbd21da8cb60bd759e8d8f3d7831e6f62883
vishalbpatil1 <52704376+vishalbpatil1@users.noreply.github.com>
2020-06-07 18:34:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import plotly.express as px
 df = px.data.iris()
 fig = px.scatter(df, x="sepal_width", y="sepal_length")
-fig.show()
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
