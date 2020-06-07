37e0654fcb94162305d926094e676962e4a64209
vishalbpatil1 <52704376+vishalbpatil1@users.noreply.github.com>
2020-06-07 18:10:46 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+import plotly.express as px
+df = px.data.iris()
+import plotly.graph_objects as go # or plotly.express as px
+#fig = go.Figure() # or any Plotly Express function e.g. px.bar(...)
+fig = px.scatter(df, x="sepal_width", y="sepal_length")
+fig.show()
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+app = dash.Dash()
+app.layout = html.Div([
+    dcc.Graph(figure=fig)
+    
+])
+
+if __name__ == '__main__':
+    app.run_server(host='0.0.0.0', port=8080)
