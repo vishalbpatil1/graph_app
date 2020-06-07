99e74b9d9e3560d28fb8a17cd84306da610997b7
vishalbpatil1 <52704376+vishalbpatil1@users.noreply.github.com>
2020-06-07 18:22:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 import plotly.express as px
 df = px.data.iris()
-import plotly.graph_objects as go # or plotly.express as px
-#fig = go.Figure() # or any Plotly Express function e.g. px.bar(...)
 fig = px.scatter(df, x="sepal_width", y="sepal_length")
 fig.show()
 import dash
