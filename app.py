f20f7854c6210f76f40548a4edcb52798bfb3f9e
vishalbpatil1 <52704376+vishalbpatil1@users.noreply.github.com>
2020-06-07 18:39:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,6 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 app = dash.Dash(__name__)
-app.layout = html.Div([
-    dcc.Graph(figure=fig)
-    
-])
-
+app.layout = html.Div([ dcc.Graph(figure=fig)])
 if __name__ == '__main__':
-    app.run_server()
+    app.run_server(debug=Flase)
