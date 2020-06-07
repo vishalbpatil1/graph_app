9088464ff0b9ac6ac53c320a4ee22e963b575aa7
vishalbpatil1 <52704376+vishalbpatil1@users.noreply.github.com>
2020-06-07 20:01:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,33 @@
-import plotly.express as px
-df = px.data.iris()
-fig = px.scatter(df, x="sepal_width", y="sepal_length")
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
-app = dash.Dash(__name__)
-server=app.server
-app.layout = html.Div([ dcc.Graph(figure=fig)])
+app = dash.Dash()
+
+app.layout = html.Div(
+    html.Div([
+        html.H1(children='Hello World'),
+
+        html.Div(children='''
+            Dash: A web application framework for Python.
+        '''),
+
+        dcc.Graph(
+            id='example-graph',
+            figure={
+                'data': [
+                    {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                    {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+                ],
+                'layout': {
+                    'title': 'Dash Data Visualization'
+                }
+            }
+        ),
+
+    ])
+)
+
 if __name__ == '__main__':
-    app.run_server(debug=Flase)
+    app.run_server(debug=True)
